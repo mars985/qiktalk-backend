4bdf791b9855e14923c257cb2cf5fd04ea088e96
mars985 <miratparmar13@gmail.com>
2025-07-24 12:19:29 +0530

    basic socket.io

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-const express = require('express');
+const express = require("express");
+const { Server } = require("socket.io");
+const { createServer } = require("http");
+const path = require("path");
 
 const app = express();
-const path = require('path');
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-const userAPI = require('./api/userAPI');
-const messageAPI = require('./api/messageAPI');
+// root
+app.get("/", (req, res) => {
+  res.send("hey");
+});
 
-app.get('/', (req, res) => {
-    res.send('hey');
-})
+// CRUD routes
+const userAPI = require("./api/userAPI");
+const messageAPI = require("./api/messageAPI");
 
 userAPI(app);
 messageAPI(app);
 
-app.listen(3000);
+// Socket.io
+io.on("connect", (socket) => {
+  console.log("A user connected" + socket.id);
+});
+
+
+server.listen(3000);
